1356ee0228e8bd76dac28ef623b8594c366749ba
blakjay116 <maloudajohn@gmail.com>
2023-04-19 17:06:22 +0100

    comments added

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ const session = require("express-session");
 const store = new session.MemoryStore();
 const bodyParser = require("body-parser");
 const app = express();
+
 const LocalStrategy = require("passport-local").Strategy;
 app.set("view engine", "ejs");
+
+//import the util folder that contains helper functions for logging, findByUsername and creating account
 const db = require("./util")
+
+
 app.use(bodyParser.json());
 bodyParser.urlencoded({ extended: true });
 app.use(
@@ -24,13 +29,18 @@ app.use(
 
 passport.use(
   new LocalStrategy((username, password, done) => {
+    //check if a user with username exists
     let user = db.findByUsername(username);
+
+
      if (!user) {
        console.log("user does not exist");
        return done(null, false);
+       //log user does not exist to console if user does not exist.. ie if findByUsername returns false
      }
 
      if (password !== user.password) {
+       // check for password match
        console.log("invalid password, try again");
        return done(null, false);
      }
@@ -64,12 +74,17 @@ app.get("/register", (req, res) => {
 
 
 app.post("/register", async (req, res) => {
+  // extract username and password form req.body
   const { username, password } = req.body;
 
+
+//check if a user with the username exists, and redirect to login if it exists
   const member = db.findByUsername(username)
   if (member) {
-    res.render("login");
+    res.redirect("/login");
   }else{
+
+    //createNewUser and push to the records array and redirect to login
     db.records.push(db.createNewUser(username, password));
    res.redirect("/login");
   }
